4d43baccb69e48e43cae58dfefc3c683a4536119
michael burney <burney318@gmail.com>
2016-04-28 20:36:48 -0500

    cleaning up login

--- app/views/login.blade.php
+++ app/views/login.blade.php
@@ -6,20 +6,14 @@
 
 @extends('layouts.master')
 	@section('content')
-
 		<body id="landingBody">
 			<div class="container"> 
 				<div class="whiteBox">			
-					<div class="title"> 
-						programmerPrep
-					</div>
-					<p class="definition"> 
-						Connecting aspiring Jr. Developers to Mentors in the industry
-					</p>
+					<div class="title"> programmerPrep</div>
+					<p class="definition"> Connecting aspiring Jr. Developers to Mentors in the industry</p>
 
 					<!-- login -->
 					<form method="POST" id="signupForm">
-					<div class="loginField">
 						<input id="username" name="username" type="text" Placeholder="username"><br>
 		        		<input id="password" name="password" type="password" placeholder="password"></br>
 		        		<div class="registration">
@@ -31,17 +25,18 @@
 		        			<button type="submit" id="loginButton" class="loginFormButton">Login</button>
 		      			</div>
 		          	</form>         
+		          	</div>
 
 		          	 <!-- you need to add the spine (absolute 0 0 )  -->
 
-			<script>
-			$("#registerButton").click(function() {
-		    	$(".registration").slideToggle("invisible");
-					// Toggle Register/Un-Register
-		    		$(this).text(function(i,v) {
-		        		return v === '  Back  ' ? 'Register' : '  Back  '
-		    		});
-			});
-	</script>
+<script>
+// Toggle Register/Un-Register
+$("#registerButton").click(function() {
+	$(".registration").slideToggle("invisible");
+		$(this).text(function(i,v) {
+    		return v === '  Back  ' ? 'Register' : '  Back  '
+		});
+});
+</script>
 @stop
 
