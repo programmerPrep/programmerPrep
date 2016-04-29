26e665428def5bba1b5b950ca9e11ac4dc9d62f5
michael burney <burney318@gmail.com>
2016-04-29 18:56:17 -0500

    commiting sitkcy note and landing page after math

--- app/views/dashboard.blade.php
+++ app/views/dashboard.blade.php
@@ -1,1 +1,21 @@
-test
+@extends('layouts.master')
+@section('content')
+
+<body>
+
+<ul>
+	<li> home </li>
+	<li> calender</li>
+	<li> imaginaryG+Button</li>
+	<li> allMentors</li>
+
+	<li> logOut</li>
+	<li> aboutUs</li>
+</ul>
+
+
+
+
+
+</body>
+@stop
--- app/views/login.blade.php
+++ app/views/login.blade.php
@@ -3,7 +3,6 @@
 <body id="landingBody">
 	<div class="container"> 
 		<div class="whiteBox">			
-			<legend> img_AU </legend>
 			<div class="title"> programmerPrep </div>
 			<p class="definition">Connecting aspiring Jr. Developers to Mentors in the industry</p>
 
@@ -15,10 +14,8 @@
 			<div class="loginField">
 				<!--<input id="username" name="username" type="text" Placeholder="username"><br>-->
 
-				<div id="username">
-				{{ Form::text('username', null, array('class' => 'form-control', 'placeholder' => 'Username', 'id'=>'username')) }}
-				</div>
-
+				{{ Form::text('username', null, array('class' => 'form-control', 'placeholder' => 'Username', 'id'=>'username')) }}	
+			
 				<!--<input id="password" name="password" type="password" placeholder="password"></br>-->        
 				{{ Form::password('password', array('placeholder' => 'Password', 'class' => 'form-control', 'id'=> 'password')) }}
 
@@ -33,11 +30,12 @@
 				<button type="button" id="registerButton" class="loginFormButton">Register</button>
 				{{-- <button type="submit" id="loginButton" class="loginFormButton">Login</button> --}}
 				{{ Form::submit('Sign in', array('class' => 'btn-log')); }}
-			</div>
-			</div>
-			</div>
-			</div>
+											<img class="stickyNote" src="/img/stickyNote.png">
 
+			</div>
+			</div>
+			</div>
+			</div>
 				<!--</form> -->    
 				{{ Form::close() }}         
 				<!-- you need to add the spine (absolute 0 0 )  -->
