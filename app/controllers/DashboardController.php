fcfa8436110eca09a0fda3b3e2fcbf04e4214f71
PlatonicPoohBear <biologynut10@hotmail.com>
2016-04-30 19:27:01 -0500

    Made controller function to query db for mentor index. Still needs search.

--- app/controllers/DashboardController.php
+++ app/controllers/DashboardController.php
@@ -2,58 +2,67 @@
 
 class DashboardController extends \BaseController {
 
-    public function show($id){
-    $user = User::find($id);
-    $is_mentor = User::checkIfMentor($user);
+    public function show($id)
+    {
+        $user = User::find($id);
+        $is_mentor = User::checkIfMentor($user);
 
-    if ($is_mentor){
-     
-        $relationship = DB::table('relationships')->where('mentor_id', $id)->get();
-        $pending = [];
-        $active = [];
+        if ($is_mentor){
+         
+            $relationship = DB::table('relationships')->where('mentor_id', $id)->get();
+            $pending = [];
+            $active = [];
 
-        
-        foreach($relationship as $value) {
+            
+            foreach($relationship as $value) {
 
-            if ($value->is_pending == 1) {
-                $temp = $value->student_id;
-                $temp = DB::table('users')->where('id', $temp)->first();
-                $pending[]= $temp;
-            } elseif ($value->is_pending == 0) {
-                $temp = $value->student_id;
-                $temp = DB::table('users')->where('id', $temp)->first();
-                $active[]= $temp;
+                if ($value->is_pending == 1) {
+                    $temp = $value->student_id;
+                    $temp = DB::table('users')->where('id', $temp)->first();
+                    $pending[]= $temp;
+                } elseif ($value->is_pending == 0) {
+                    $temp = $value->student_id;
+                    $temp = DB::table('users')->where('id', $temp)->first();
+                    $active[]= $temp;
+                }
             }
-        }
-        
-     // var_dump($user->students());
-     // var_dump($relationship = Relationship::where('mentor_id', '=', $id)->get());
+            
+         // var_dump($user->students());
+         // var_dump($relationship = Relationship::where('mentor_id', '=', $id)->get());
 
-    } else {
+        } else {
 
-        $relationship = DB::table('relationships')->where('student_id', $id)->get();
-        $pending = [];
-        $active = [];
-        
-        foreach($relationship as $value) {
+            $relationship = DB::table('relationships')->where('student_id', $id)->get();
+            $pending = [];
+            $active = [];
+            
+            foreach($relationship as $value) {
 
-            if ($value->is_pending == 1) {
-                $temp = $value->mentor_id;
-                $temp = DB::table('users')->where('id', $temp)->first();
-                $pending[]= $temp;
-            } elseif ($value->is_pending == 0) {
-                $temp = $value->mentor_id;
-                $temp = DB::table('users')->where('id', $temp)->first();
-                $active[]= $temp;
+                if ($value->is_pending == 1) {
+                    $temp = $value->mentor_id;
+                    $temp = DB::table('users')->where('id', $temp)->first();
+                    $pending[]= $temp;
+                } elseif ($value->is_pending == 0) {
+                    $temp = $value->mentor_id;
+                    $temp = DB::table('users')->where('id', $temp)->first();
+                    $active[]= $temp;
+                }
             }
-        }
 
-    
-        // var_dump($user->mentors());
-        // var_dump($relationship = Relationship::where('student_id', '=', $id)->get());
+        
+            // var_dump($user->mentors());
+            // var_dump($relationship = Relationship::where('student_id', '=', $id)->get());
+        }
+        return View::make('test')->with(array('pending' => $pending, 'active' => $active));
+
     }
-    return View::make('test')->with(array('pending' => $pending, 'active' => $active));
 
+
+    public function mentorIndex()
+    {
+        $mentors = DB::table('users')->where('is_mentor', 1)->get();
+
+        return View::make('mentor_index_test')->with('mentors', $mentors);
     }
 }
 
