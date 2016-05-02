026aa740e03c29249f28ad8e17cc8b688bc91d47
michael burney <burney318@gmail.com>
2016-05-02 09:50:10 -0500

    commiting aboutus.blade.php

--- app/views/aboutUs.blade.php
+++ app/views/aboutUs.blade.php
@@ -1,1 +1,53 @@
-aboutUs.blade.php
+@extends('layouts.master')
+@section('content')
+
+
+<main>
+
+	<div class="container">
+		<div class="row">
+			<!-- 4 pictures of us -->
+			<img src="/img/burney.jpg">
+			email github linkdin
+			front end
+
+			<img src="/img/greg.jpg">
+			email github linkdin
+			front end
+
+			<img src="/img/logan.jpg">
+			email github linkdin
+			back end
+
+			<img src="/img/nick.jpg">
+			email github linkdin
+			back end
+		</div>
+	</div>
+</main>
+<body>
+	<div class="row">
+		big ass paragraph about what we did and who we are and what we stand for and what we do nigga Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+		tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+		quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+		consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+		cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+		proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+	</div>
+
+
+
+
+
+
+</body>
+
+
+
+
+
+
+
+
+</div>
+@stop
