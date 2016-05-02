675082904be5898ad33e10ce730ab672b86e4988
michael burney <burney318@gmail.com>
2016-05-02 09:51:26 -0500

    commiting what i have

--- app/views/aboutUs.blade.php
+++ app/views/aboutUs.blade.php
@@ -27,7 +27,7 @@
 </main>
 <body>
 	<div class="row">
-		big ass paragraph about what we did and who we are and what we stand for and what we do nigga Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+		Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
 		tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
 		quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
 		consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
