77f1fe8ead0b2dc92d51fd2b36f4c4700dba07ef
michael burney <burney318@gmail.com>
2016-05-02 14:38:01 -0500

    NEED: linkdins

--- app/views/aboutUs.blade.php
+++ app/views/aboutUs.blade.php
@@ -8,39 +8,36 @@
 			    <div class="three columns">
 					<img class="picture" src="/img/burney.jpg">
 					<br>Front-End <br>
-					<img class="icon" src="/icons/git.png">
+					<a href="https://github.com/burneyburney"><img class="icon" src="/icons/git.png"></a>
 					<img class="icon" src="/icons/link.png">
-					<img class="icon" src="/icons/mail.png">
+					<a href="mailto:Burney318@gmail.com?Subject="programmerPrep!" target="_top"><img class="icon" src="/icons/mail.png"></a>	
 				</div>
 			</div>
-
 			<div class="greg">
 				<div class="three columns">
 					<img class="picture"src="/img/greg.jpg">
 					<br>Front-End<br>
-					<img class="icon" src="/icons/git.png">
+					<a href="https://github.com/Gregory-Bickham"><img class="icon" src="/icons/git.png"></a>
 					<img class="icon" src="/icons/link.png">
-					<img class="icon" src="/icons/mail.png">
+					<a href="mailto:Gregory.Bickham@gmail.com?Subject="I%20found%20your%20website!" target="_top"><img class="icon" src="/icons/mail.png"></a>				
 				</div>
 			</div>
-
 			<div class="logan">
 				<div class="three columns">
 					<img class="picture" id="logan" src="/img/logan.jpg">
 					<br>Back-End<br>
-					<img class="icon" src="/icons/git.png">
+					<a href="https://github.com/yungshark"><img class="icon" src="/icons/git.png"></a>
 					<img class="icon" src="/icons/link.png">
-					<img class="icon" src="/icons/mail.png">
+					<a href="mailto:thevisagiftcard@gmail.com?Subject="I%20found%20your%20website!" target="_top"><img class="icon" src="/icons/mail.png"></a>				
 				</div>
 			</div>
-
 			<div class="nick">
 				<div class="three columns">
 					<img class="picture" src="/img/nick.jpg">
 					<br>Back-End<br>
-					<img class="icon" src="/icons/git.png">
+					<a href="https://github.com/platonicpoobear"><img class="icon" src="/icons/git.png"></a>
 					<img class="icon" src="/icons/link.png">
-					<img class="icon" src="/icons/mail.png">
+					<a href="mailto:nicholas.vrzalik@hotmail.com?Subject="I%20found%20your%20website!" target="_top"><img class="icon" src="/icons/mail.png"></a>				
 				</div>
 			</div>
 		</div>
@@ -51,6 +48,4 @@
 		The creation of programmerPrep revolved around the fact that there was no clear path written out for us (future jr. developers). We all have the same mantra: Eat, Sleep, Code, Repeat but what's there to steer us in the right direction career wise? Who do we turn to incase of a specific issue? How do we get our foot in the door at that one place? So we made a hub for that. The purpose is to aquire a mentor in the community to usher you in. That's why we created programmerPrep. Day-in and day-out for two weeks the four of us worked the front and the back of this web-application to create a truely one of a kind software as a service. Not just for the four of us, but for the everyone that wanted to join in the "skill pass down" that the tech scene is today.
 	</div>
 </body>
-
-
 @stop
