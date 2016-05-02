91d8d87f574dadf09726f20af19e0ecfc32ce38f
michael burney <burney318@gmail.com>
2016-05-02 12:36:29 -0500

    working with about us... my pc is running slow so commiting just incase

--- app/views/aboutUs.blade.php
+++ app/views/aboutUs.blade.php
@@ -6,37 +6,46 @@
 	<!-- <div class="container"> -->
 		<div class="row">
 			<!-- 4 pictures of us -->
-		    <div class="three columns">
-				<img class="picture" src="/img/burney.jpg">
-				<img class="icon" src="/icons/git.png">
-				<img class="icon" src="/icons/link.png">
-				<img class="icon" src="/icons/mail.png">
-				front end
+			<div class="burney">
+			    <div class="three columns">
+					<img class="picture" src="/img/burney.jpg">
+					<br>front end <br>
+					<img class="icon" src="/icons/git.png">
+					<img class="icon" src="/icons/link.png">
+					<img class="icon" src="/icons/mail.png">
+				</div>
 			</div>
 
-			<div class="three columns">
-				<img class="picture"src="/img/greg.jpg">
-				<img class="icon" src="/icons/git.png">
-				<img class="icon" src="/icons/link.png">
-				<img class="icon" src="/icons/mail.png">
-				front end
+			<div class="greg">
+				<div class="three columns">
+					<img class="picture"src="/img/greg.jpg">
+					<br>front end<br>
+					<img class="icon" src="/icons/git.png">
+					<img class="icon" src="/icons/link.png">
+					<img class="icon" src="/icons/mail.png">
+				</div>
 			</div>
 
-			<div class="three columns">
-				<img class="picture" id="logan" src="/img/logan.jpg">
-				<img class="icon" src="/icons/git.png">
-				<img class="icon" src="/icons/link.png">
-				<img class="icon" src="/icons/mail.png">
-				back end
+			<div class="logan">
+				<div class="three columns">
+					<img class="picture" id="logan" src="/img/logan.jpg">
+					<br>back end<br>
+					<img class="icon" src="/icons/git.png">
+					<img class="icon" src="/icons/link.png">
+					<img class="icon" src="/icons/mail.png">
+				</div>
 			</div>
 
-			<div class="three columns">
-				<img class="picture" src="/img/nick.jpg">
-				<img class="icon" src="/icons/git.png">
-				<img class="icon" src="/icons/link.png">
-				<img class="icon" src="/icons/mail.png">
-				back end	
+			<div class="nick">
+				<div class="three columns">
+					<img class="picture" src="/img/nick.jpg">
+					<br>back end<br>
+					<img class="icon" src="/icons/git.png">
+					<img class="icon" src="/icons/link.png">
+					<img class="icon" src="/icons/mail.png">
+				</div>
 			</div>
+		</div>
 	</div>
 </main>
 <body id="aboutUsBody">
