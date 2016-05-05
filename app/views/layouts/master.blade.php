6b112df3b4363b7c7bbf89f0460470dbdd34741d
GBickham <Bickham@macs-MacBook-Pro.local>
2016-05-05 14:43:54 -0500

    Fixing stuff

--- app/views/layouts/master.blade.php
+++ app/views/layouts/master.blade.php
@@ -10,25 +10,30 @@
         <!-- skeleton CDN -->
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/skeleton/2.0.4/skeleton.min.css">
         <!--  external landing css-->
-        <link rel="stylesheet" type="text/css" href="./css/login.css">
-        <!-- jQuery CDN -->
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
-        <!-- jQuery Validation CDN -->
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.15.0/jquery.validate.min.js"></script>
-        <!-- frontEndValidation -->
-        <script src="js/frontEndValidation.js"></script>
-        <!-- external dashboard css -->
-        <link rel="stylesheet" type="text/css" href="./css/dashboard.css">
-        <!-- Nav Bar -->
-        <link rel="stylesheet" type="text/css" href="./css/navbar.css">
+        <link rel="stylesheet" type="text/css" href="/css/login.css">
         <!-- AboutUs css -->
-        <link rel="stylesheet" type="text/css" href="./css/aboutUs.css">
+        <!-- <link rel="stylesheet" type="text/css" href="/css/aboutUs.css"> -->
+
+        <!-- external dashboard css -->
+        <link rel="stylesheet" type="text/css" href="/css/dashboard.css">
+        
+        
         <!--  testedit.blade.php css -->
-        <link rel="stylesheet" type="text/css" href="./css/edit.css">
+        <link rel="stylesheet" type="text/css" href="/css/edit.css">
+        <!-- Nav Bar -->
+        <link rel="stylesheet" type="text/css" href="/css/navbar.css">
 
         @yield('top-script')
     </head>
+    <body>
         <main class="container">
             @yield('content')
-        </main>      
+        </main>
+            <!-- jQuery CDN -->
+          <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
+        <!-- jQuery Validation CDN -->
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.15.0/jquery.validate.min.js"></script>
+        <!-- frontEndValidation -->
+        <script src="js/frontEndValidation.js"></script>      
+</body>
 </html>
