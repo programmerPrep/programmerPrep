c4942d36f351957fa58c86cc64fe789ecf959853
Logan Farmer <thevisagiftcard@gmail.com>
2016-05-07 13:31:53 -0500

    fixed login page

--- app/views/layouts/master.blade.php
+++ app/views/layouts/master.blade.php
@@ -38,17 +38,16 @@
         <!-- Nav Bar -->
         <link rel="stylesheet" type="text/css" href="/css/navbar.css">
 
-        @yield('top-script')
-    </head>
-    <body>
-        <main class="container">
-            @yield('content')
-        </main>
             <!-- jQuery CDN -->
           <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
         <!-- jQuery Validation CDN -->
         <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.15.0/jquery.validate.min.js"></script>
         <!-- frontEndValidation -->
         <script src="/js/frontEndValidation.js"></script>      
-</body>
+
+        @yield('top-script')
+    </head>
+        <main class="container">
+            @yield('content')
+        </main>
 </html>
