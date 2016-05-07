54c1c8830c447c0b79e728494704890230a50563
michael burney <burney318@gmail.com>
2016-05-07 17:05:41 -0500

    alert messages, about us, testing these t hings, moving forward

--- app/views/aboutUs.blade.php
+++ app/views/aboutUs.blade.php
@@ -56,7 +56,47 @@
 		We all have the same mantra: Eat, Sleep, Code, Repeat but what's there to steer us in the right direction career wise? Who do we turn to incase of a specific issue? How do we get our foot in the door at that one place? So we made a hub for that. 
 
 		The purpose is to aquire a mentor in the community to usher you in. That's why we created programmerPrep. Day-in and day-out for two weeks the four of us worked the front and the back of this web-application to create a truely one of a kind software as a service.
+---------------------------------------------------------------------------------------------------
+		The creation of programmerPrep revolved around two main goals. One, A way to give back to the community that spawned us, and two, providing additional education to the masses. We all have the same 
 
+		We all have the same mantra: Eat, Sleep, Code, Repeat but what's there to steer us in the right direction career wise? Who do we turn to incase of a specific issue? How do we get our foot in the door at that one place? What Python framework should I learn first? This is where programmerPrep steps in. The four of us, above, wanted to give back to the community that spawned us. We worked for two tireless weeks using CSS frameworks, PHP, Laravel, and jQuery to deliver, unique software as a service, to the community
+
+		use this to continue learning
+
+		So we made a hub for that. 
+
+		The purpose is to aquire a mentor in the community to usher you in. That's why we created programmerPrep. Day-in and day-out for two weeks the four of us worked the front and the back of this web-application to create a truely one of a kind software as a service.
+
+		In the end, it’s all about you. As we evolve and educate ourselves, we will seek to perfect this safe
+
+		Not just for the four of us, but for the everyone that wanted to join in the "skill pass down" that the tech scene is today.
 	</div>
 </body>
 @stop
+
+describe this web application
+	software as a service - 
+
+	programmerPrep was created with one main goal in mind and that was to create a web-application to connect developers together.
+
+	
+tells the users who we are
+	we are web developers
+	we wanted to give back to the community that spawned us
+
+talks about the sites technologies
+	css frameworks
+	 skeleton
+	 normalize
+	 favicon()
+	 php
+	 laravel
+	 javascript
+	 jQuery
+
+How we made it
+	Eat sleep code repeat
+
+
+
+
--- app/views/layouts/master.blade.php
+++ app/views/layouts/master.blade.php
@@ -8,38 +8,28 @@
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/4.1.1/normalize.min.css">
         <!-- skeleton CDN -->
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/skeleton/2.0.4/skeleton.min.css">
-
         <!--  external landing css-->
         <link rel="stylesheet" type="text/css" href="/css/login.css">
-
-        
         <!-- external dashboard css -->
         <link rel="stylesheet" type="text/css" href="/css/dashboard.css">
         <!-- NavBar -->
         <link rel="stylesheet" type="text/css" href="/css/navbar.css">
-        
         <!-- Font Awesome -->
         <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css">
         <!-- Fonts -->
         <link rel="stylesheet" type="text/css" href="/css/fonts.css">
-        
         <!-- Nav Bar -->
         <link rel="stylesheet" type="text/css" href="/css/navbar.css">
-
         <!-- AboutUs css -->
         <link rel="stylesheet" type="text/css" href="/css/aboutUs.css">
-
         <!-- external dashboard css -->
         <link rel="stylesheet" type="text/css" href="/css/dashboard.css">
-        
-        
         <!--  testedit.blade.php css -->
         <link rel="stylesheet" type="text/css" href="/css/edit.css">
         <!-- Nav Bar -->
         <link rel="stylesheet" type="text/css" href="/css/navbar.css">
-
-            <!-- jQuery CDN -->
-          <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
+        <!-- jQuery CDN -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
         <!-- jQuery Validation CDN -->
         <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.15.0/jquery.validate.min.js"></script>
         <!-- frontEndValidation -->
@@ -49,10 +39,10 @@
     </head>
         <main class="container">
             @if (Session::has('successMessage'))
-                <div class="alert alert-success">{{{ Session::get('successMessage') }}}</div>
+                <div class="notificationMessage" id="successMessage">{{{ Session::get('successMessage') }}}</div>
             @endif
             @if (Session::has('errorMessage'))
-                <div class="alert alert-danger">{{{ Session::get('errorMessage') }}}</div>
+                <div class="notificationMessage" id="errorMessage">{{{ Session::get('errorMessage') }}}</div>
             @endif
             @yield('content')
         </main>
