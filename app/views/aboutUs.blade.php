b94f503971566436890b8f0b3faf6638efa6e6fe
michael burney <burney318@gmail.com>
2016-05-09 14:28:02 -0500

    login>?

--- app/views/aboutUs.blade.php
+++ app/views/aboutUs.blade.php
@@ -46,7 +46,7 @@
 <body class="JAH" id="aboutUsBody">
 	<div class="row">
 		<div id="aboutUsParagraph">
-			We are a team of web developers, with a desire for knowledge who wanted to give back to the community that spawned us. Programmer Prep was created with one main goal in mind and that was to create a web-application that connects developer to developers. We used a combination of languages and frameworks to dial in our desired results: Skeleton for mobile responsiveness and normalize to ensure that all browsers render the website consistently. PHP, Laravel and MySQL were used on the backend. JavaScript and jQuery were used for simple logic for the front end. But even with this slew of technologies this could not have been done if it wern't for the mantra "Eat Sleep Code Repeat"	
+			We are a team of web developers, with a desire for knowledge who wanted to give back to the community that spawned us. Programmer Prep was created with one main goal in mind and that was to create a web-application that connects developer to developers. We used a combination of languages and frameworks to dial in our desired results: Skeleton for mobile responsiveness and normalize to ensure that all browsers render the website consistenty. PHP, Laravel and MySQL were used on the backend. JavaScript and jQuery were used for simple logic for the front end. But even with this slew of technologies this could not have been done if it wern't for the mantra "Eat, Sleep, Code, Repeat"	
 		</div>
 	</div>
 </body>
