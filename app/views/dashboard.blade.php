84f878ea33441f06a026f5a95643d3a31622e81f
GBickham <Bickham@macs-MacBook-Pro.local>
2016-05-10 11:05:29 -0500

    Minor changes

--- app/views/dashboard.blade.php
+++ app/views/dashboard.blade.php
@@ -11,11 +11,12 @@
 				@foreach ($pending as $user)
 					<?php $interestsArray = explode(',', $user->interests); ?>
 					<div class="note yellow">
-
-						<a href="#" target="_top"><img class="addIcon" src="/icons/add1.svg" alt="Add Mentor!"></a>
-	    					{{ HTML::image($user->img_url, $user->first_name, array('class' => 'statusPic')) }}
-
-						<div id="pending">Pending</div>
+						<div class="align-left">
+							<a href="{{{action('UsersController@acceptRequest', $user->id)}}}">
+								<img src="/icons/glyphicons/png/glyphicons-191-plus-sign.png">
+							</a>
+							<div id="pending">Pending</div>
+						</div>
 	    				{{ HTML::image($user->img_url, $user->first_name, array('class' => 'statusPic')) }}
 	  					<div class="quote-container">
 	    					{{{ $interestsArray[0] }}}, {{{ $interestsArray[1] }}}, {{{ $interestsArray[2] }}}...
@@ -30,20 +31,21 @@
 			<div class="six columns">
 				@foreach ($active as $user)
 					<?php $interestsArray = explode(',', $user->interests); ?>
-		    		<div class="note yellow">
-
-		    		<a href="#" target="_top"><img class="subIcon" src="/icons/remove2.png" alt="Add Mentor!"></a>
-
-		    			<div id="active">Active</div>
-
-		    			{{ HTML::image($user->img_url, $user->first_name, array('class' => 'statusPic')) }}
-		  				<div class="quote-container">
-		    				{{{ $interestsArray[0] }}}, {{{ $interestsArray[1] }}}, {{{ $interestsArray[2] }}}...
-		    				<div class="author yunusStatus">
-		    					{{ $user->first_name }} {{ $user->last_name }}
+					<div class="note yellow">
+						<div class="align-left">
+							<a href="{{{action('UsersController@show', $user->id)}}}">
+								<img src="/icons/glyphicons/png/glyphicons-193-remove-sign.png">
+							</a>
+							<div id="active">Active</div>
+						</div>
+	    				{{ HTML::image($user->img_url, $user->first_name, array('class' => 'statusPic')) }}
+	  					<div class="quote-container">
+	    					{{{ $interestsArray[0] }}}, {{{ $interestsArray[1] }}}, {{{ $interestsArray[2] }}}...
+	    					<div class="author yunusStatus">
+	    						{{ $user->first_name }} {{ $user->last_name }}
 								<a href="mailto:{{ $user->email }}?Subject='programmerPrep!'" target="_top"><img class="icon" src="/icons/mail.png"></a>
-		    				</div>
-		  				</div>
+	    					</div>
+	  					</div>
 					</div>    	
 				@endforeach
 			</div>
