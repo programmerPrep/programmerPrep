2a71d38ecc2d15d4c4b26f534f0529e09df73364
michael burney <burney318@gmail.com>
2016-05-10 11:06:50 -0500

    added about us, edited profile page, and edit page

--- app/views/Partials/navbar.blade.php
+++ app/views/Partials/navbar.blade.php
@@ -3,7 +3,7 @@
 			<li><a href="{{{ action('DashboardController@show', Auth::id()) }}}">Home</a></li>
 			<li><a href="https://calendar.google.com/calendar/render#main_7">Calendar</a></li>
 			<li><a href="https://hangouts.google.com/">Video Chat</a></li>
-			<li><a href="{{{ action('UsersController@show', Auth::id()) }}}">Profile</a></li>
+			<li><a href="{{{ action('UsersController@edit', Auth::id()) }}}">Profile</a></li>
 			<li><a href="{{{ action('UsersController@index') }}}">Mentors</a></li>
 			<li><a href="{{{ action('DashboardController@aboutUs') }}}">About Us</a></li>
 			<li><a href="{{{ action('UsersController@showLogout') }}}">Log Out</a></li>
